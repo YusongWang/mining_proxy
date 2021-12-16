d6bcba239e9ab4562896dd277a9e84185528cf75
Wangyusong <admin@wangyusong.com>
2021-12-16 10:48:14 +0800

    变更为下一个版本

---
+++ src/client/worker.rs
@@ -0,0 +1,8 @@
+pub struct worker {
+    pub worker_name :String,
+    pub worker_wallet :String,
+    pub full_name:String,
+    pub share_count:u64,
+    pub accept_count:u64,
+    pub invalid_count:u64,
+}
