2bcfbff706a00aab50a6027b2f8d785f5b4de02e
Wangyusong <admin@wangyusong.com>
2021-12-17 09:53:49 +0800

    修改SSL版本

--- src/mine/develop.rs
+++ src/mine/develop.rs
@@ -307,6 +307,8 @@ impl Mine {
         };
         let login_msg = serde_json::to_string(&login)?;
         send.send(login_msg).unwrap();
+
+        sleep(std::time::Duration::new(1, 0)).await;
         //RECV the work.Login package success or fail
         let eth_get_work = ClientGetWork {
             id: 5,
