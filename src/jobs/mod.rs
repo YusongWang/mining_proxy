839f03c65c5704072ab175aa2df8f31156db2fe6
Yusong Wang <admin@wangyusong.com>
2021-12-23 23:30:24 +0800

    添加jobs

---
+++ src/jobs/mod.rs
@@ -0,0 +1,57 @@
+use async_channel::{bounded, Receiver, Sender, TrySendError};
+
+pub struct Job {
+    phread_id: u32,
+    job: String,
+}
+impl Job {
+    pub fn get_job(&mut self) -> String {
+        self.job.clone()
+    }
+    pub fn get_id(&mut self) -> u32 {
+        self.phread_id
+    }
+
+    pub fn new(id: u32, job: String) -> Job {
+        Job { job, phread_id: id }
+    }
+}
+//c: Sender<i32>, r: Receiver<i32>
+pub struct JobQueue {
+    tx: Sender<Job>,
+    rx: Receiver<Job>,
+}
+
+impl JobQueue {
+    pub fn new(cap: usize) -> Self {
+        let (tx, rx) = bounded::<Job>(cap);
+        Self { tx, rx }
+    }
+
+    pub fn recv(&self) -> Option<Job> {
+        if let Ok(job) = self.rx.try_recv() {
+            return Some(job);
+        }
+        None
+    }
+
+    pub fn send(&self, job: Job) -> Option<()> {
+        match self.tx.try_send(job) {
+            _ => {}
+            Err(e) => match e {
+                TrySendError::Full(job) => {
+                    if let Ok(_) = self.rx.try_recv() {
+                        return self.send(job);
+                    } else {
+                        return None;
+                    }
+                }
+                TrySendError::Closed(i) => {
+                    //println!("线程异常退出了");
+                    return None;
+                }
+            },
+        }
+        Some(())
+    }
+}
