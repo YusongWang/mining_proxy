af12f5d3a29be25e4b926f9f78e3249d342d09c8
Yusong Wang <admin@wangyusong.com>
2022-01-07 00:22:58 +0800

    更新一键安装脚本

--- install.sh
+++ install.sh
@@ -1,116 +1,65 @@
-#!/bin/sh
-# shellcheck shell=dash
-#[[ $(id -u) != 0 ]] && echo -e "\n 请使用 ${red}root ${none}用户运行 ${yellow}~(^_^) ${none}\n" && exit 1
-red='\e[91m'
-green='\e[92m'
-yellow='\e[94m'
-magenta='\e[95m'
-cyan='\e[96m'
-none='\e[0m'
-_red() { echo -e ${red}$*${none}; }
-_green() { echo -e ${green}$*${none}; }
-_yellow() { echo -e ${yellow}$*${none}; }
-_magenta() { echo -e ${magenta}$*${none}; }
-_cyan() { echo -e ${cyan}$*${none}; }
+#!/bin/bash
+echo -n "输入矿工名:"
+read workname
+echo -n "输入代理池TCP地址:"
+read pool_tcp_address
+echo -n "输入代理池SSL地址:"
+read pool_ssl_address
+echo -n "是否抽水? 0不抽水 1抽水:"
+read share
+echo -n "输入抽水池SSL地址:"
+read share_ssl_address
+echo -n "输入抽水钱包地址(0x开头):"
+read share_wallet
+echo "-----------"
+echo "Welcome,$workname!"
+rm -rf ~/proxy_tmp
+cd ~/
+mkdir ~/proxy_tmp
+cd ~/proxy_tmp
+wget -c "https://github.com/dothinkdone/minerProxy/releases/download/v0.1.8/linux.tar.gz"
+tar xvf linux.tar.gz
+rm -rf "/opt/$workname/"
+mkdir -p "/opt/$workname/bin"
+mkdir -p "/opt/$workname/config"
+mkdir -p "/opt/$workname/logs"
+
+mv proxy "/opt/$workname/bin"
+mv identity.p12 "/opt/$workname/config/"
+mv default.yaml "/opt/$workname/config/"
 
 
-if [ "$KSH_VERSION" = 'Version JM 93t+ 2010-03-05' ]; then
-    # The version of ksh93 that ships with many illumos systems does not
-    # support the "local" extension.  Print a message rather than fail in
-    # subtle ways later on:
-    echo '请使用sh 执行 !' >&2
-    exit 1
-fi
-# case $sys_bit in
-# 'amd64' | x86_64) ;;
-# *)
-#     echo -e " 
-# 	 这个 ${red}安装脚本${none} 不支持你的系统。 ${yellow}(-_-) ${none}
-# 	备注: 仅支持 Ubuntu 16+ / Debian 8+ / CentOS 7+ 系统
-# 	" && exit 1
-#     ;;
-# esac
-
-
-
-
-set -u
-
-set url = "https://github.com/dothinkdone/minerProxy/releases/download/v0.1.4/linux.tar.gz"
-
-eth_miner_config_ask() {
-    echo
-    while :; do
-        echo -e "是否开启 ETH抽水中转， 输入 [${magenta}Y/N${none}] 按回车"
-        read -p "$(echo -e "(默认: [${cyan}Y${none}]):")" enableEthProxy
-        [[ -z $enableEthProxy ]] && enableEthProxy="y"
-
-        case $enableEthProxy in
-        Y | y)
-            enableEthProxy="y"
-            eth_miner_config
-            break
-            ;;
-        N | n)
-            enableEthProxy="n"
-            echo
-            echo
-            echo -e "$yellow 不启用ETH抽水中转 $none"
-            echo "----------------------------------------------------------------"
-            echo
-            break
-            ;;
-        *)
-            error
-            ;;
-        esac
-    done
-}
-
-main() {
-    eth_miner_config_ask 
-  
-    # mkdir -p ./opt/proxy/{bin,logs,config}
-    # wget -c url
-    # tar -xvf ./opt/linux.tar.gz
-    # mv ./proxy ./opt/proxy/bin/
-    # mv ./default.yaml ./opt/proxy/config/
-    # mv ./identity.p12 ./opt/proxy/config/
-}
-
-
-gen_service() {
-    touch proxy.service
-    echo <<EOF
+cat >> /opt/$workname/config/$workname.conf << EOF
+PROXY_NAME="$workname"
+PROXY_LOG_LEVEL=1
+PROXY_LOG_PATH="/opt/$workname/logs/"
+PROXY_TCP_PORT=8800
+PROXY_SSL_PORT=14443
+PROXY_POOL_SSL_ADDRESS="asia2.ethermine.org:5555"
+PROXY_POOL_TCP_ADDRESS="asia2.ethermine.org:14444"
+PROXY_SHARE_TCP_ADDRESS="asia2.ethermine.org:14444"
+PROXY_SHARE_WALLET=""
+PROXY_SHARE_RATE=0.01
+PROXY_SHARE_NAME="ethermine_fee"
+PROXY_SHARE=2
+PROXY_P12_PATH="/var/p12/identity.p12"
+PROXY_P12_PASS="mypass"
+EOF
+cat >> /usr/lib/systemd/system/proxy.service << EOF
 [Unit]
-Description=Eth-Proxy
+Description=Proxy
 After=network.target
 After=network-online.target
 Wants=network-online.target
-
 [Service]
 Type=simple
-ExecStart=/opt/proxy/bin/proxy -c /opt/proxy/config/default.yaml
+EnvironmentFile=/opt/$workname/config/$workname.conf
+ExecStart=/opt/$workname/bin/proxy -c /opt/$workname/config/default.yaml
 ExecReload=/bin/kill -s HUP $MAINPID
 ExecStop=/bin/kill -s QUIT $MAINPID
 LimitNOFILE=65536
-WorkingDirectory=/opt/proxy/
+WorkingDirectory=/opt/$workname/
 Restart=always
-ReStartSec=600
 [Install]
 WantedBy=multi-user.target
-EOF >> proxy.service;
-}
-
-need_cmd() {
-    if ! check_cmd "$1"; then
-        err "need '$1' (command not found)"
-    fi
-}
-
-check_cmd() {
-    command -v "$1" > /dev/null 2>&1
-}
-
-
-main "$@" || exit 1
+EOF
