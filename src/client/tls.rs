bfe58ea8f83e18f6ef8b9de6c95f558f887d3e4b
Yusong Wang <admin@wangyusong.com>
2022-02-27 22:28:36 +0800

    测试新方式-等待半个小时候

--- src/client/tls.rs
+++ src/client/tls.rs
@@ -84,14 +84,14 @@ async fn transfer_ssl(
     let client_stream = tls_acceptor.accept(tcp_stream).await?;
     let (worker_r, worker_w) = split(client_stream);
     let worker_r = BufReader::new(worker_r);
-    let mut share_pool: Vec<String> = Vec::new();
+    let mut pool_address: Vec<String> = Vec::new();
     {
         let config = RwLockReadGuard::map(proxy.config.read().await, |s| s);
-        share_pool = config.share_address.to_vec();
+        pool_address = config.pool_address.to_vec();
     }
 
     let (stream_type, pools) =
-        match crate::client::get_pool_ip_and_type_from_vec(&share_pool) {
+        match crate::client::get_pool_ip_and_type_from_vec(&pool_address) {
             Ok(pool) => pool,
             Err(_) => {
                 bail!("未匹配到矿池 或 均不可链接。请修改后重试");
